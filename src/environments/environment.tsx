8f450b8edde14d48ac952f71439c90490511b5b0
Alessandro Juárez <alessj20@gmail.com>
2023-10-16 20:32:11 -0600

    Revert "auth con cookies"
    This reverts commit 5d9f9dbb24a58245a2a05a1ce1fed5c0dfc5b0cc.

--- src/environments/environment.tsx
+++ src/environments/environment.tsx
@@ -1,5 +1,4 @@
 export const environment = {
     banksUri: 'https://banks-web.vercel.app',
-    ApiUri: 'https://erp-api-v1.vercel.app',
-    domain: '.vercel.app'
+    ApiUri: 'https://erp-api-v1.vercel.app'
 }
--- src/services/Auth.service.tsx
+++ src/services/Auth.service.tsx
@@ -2,9 +2,6 @@ import axios, { AxiosResponse } from "axios";
 import { Login } from "../interfaces/Login.interface";
 import { User } from "../interfaces/User.interface";
 import { environment } from "../environments/environment";
-import CookieService from "./Cookie.service";
-
-const options: object = { expires: 7, domain: environment.domain };
 
 export default class AuthService {
   static async login(username: string, password: string): Promise<Login | null> {
@@ -15,36 +12,33 @@ export default class AuthService {
       })
       .then((response: AxiosResponse<Login>) => {
         if (response.data.code === 200 && response.data.data === true) {
-          console.log("entro al if")
-          CookieService.setCookie("isAuthenticated", "true", options);
-          console.log("seteó las primeras cookies")
-          CookieService.setCookie("user", JSON.stringify(response.data.user), options);
-          console.log("seteó las segundas cookies")
+          localStorage.setItem("isAuthenticated", "true");
+          localStorage.setItem("user", JSON.stringify(response.data.user));
           return response.data;
         } else {
-          CookieService.removeCookie("isAuthenticated");
-          CookieService.removeCookie("user");
+          localStorage.removeItem("isAuthenticated");
+          localStorage.removeItem("user");
           return response.data;
         }
       })
       .catch((error) => {
         console.error("Error al iniciar sesión:", error);
-        CookieService.removeCookie("isAuthenticated");
-        CookieService.removeCookie("user");
+        localStorage.removeItem("isAuthenticated");
+        localStorage.removeItem("user");
         return null;
       });
   }
 
   static logout(): void {
-    CookieService.removeCookie("isAuthenticated");
-    CookieService.removeCookie("user");
+    localStorage.removeItem("isAuthenticated");
+    localStorage.removeItem("user");
   }
 
   static isAuthenticated(): boolean {
-    return CookieService.getCookie("isAuthenticated") === "true";
+    return localStorage.getItem("isAuthenticated") === "true";
   }
 
   static getUser(): User {
-    return JSON.parse(CookieService.getCookie("user") as string);
+    return JSON.parse(localStorage.getItem("user") as string);
   }
 }
